57334ed908ec9157f404fa40d1a200e8d00ca85f
Soren A D <sorend@gmail.com>
2023-10-03 19:43:44 +0530

    added adpy team

--- teams/adpy/re-encrypt.sh
+++ teams/adpy/re-encrypt.sh
@@ -2,5 +2,5 @@
 
 for x in $HOME/Mega/personal/secrets/adpy/secret*.yaml; do
 	O=$(basename "$x")
-	kubeseal -f $x -o yaml --controller-name sealed-secrets -w ${O/secret/sealedsecret}
+	kubeseal -f $x -o yaml -w ${O/secret/sealedsecret}
 done
